e1bbda1a7199b32574339f3a69eb74be88699f8a
Patryk Bajak <Patryk.Bajak@microsoft.wsei.edu.pl>
2024-05-24 15:31:54 +0200

    finished lab10

--- App.js
+++ App.js
@@ -1,49 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, Image, Switch, Pressable, FlatList,ToastAndroid } from 'react-native';
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { View, Button, StyleSheet, Text, ToastAndroid, StatusBar } from 'react-native';
 import { ListItem } from './components/ListItem';
 import { Tile } from './components/Tile';
 import { MyList } from './components/MyList';
 
-export default function App(message, duration) {
-  const [selected, setSelected]= useState(new Set());
-  const photos = [
-    {id: 1, title: 'AAA', thumbnailUrl:'https://reactnative.dev/img/tiny_logo.png', width: 60, height: 60},
-    {id: 2, title: 'BBB', thumbnailUrl:'https://reactnative.dev/img/tiny_logo.png', width: 50, height: 60},
-    {id: 3, title: 'CCC', thumbnailUrl:'https://reactnative.dev/img/tiny_logo.png', width: 80, height: 80}
-  ];
-  return (
+export default function App() {
+    const [selected, setSelected] = useState(new Set());
+    const [listData, setListData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const photos = [
+        { id: 1, title: 'AAA', thumbnailUrl: 'https://reactnative.dev/img/tiny_logo.png', width: 60, height: 60 },
+        { id: 2, title: 'BBB', thumbnailUrl: 'https://reactnative.dev/img/tiny_logo.png', width: 50, height: 60 },
+        { id: 3, title: 'CCC', thumbnailUrl: 'https://reactnative.dev/img/tiny_logo.png', width: 80, height: 80 }
+    ];
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+            let data = await response.json();
+            data = data.map(item => ({
+                id: item.id,
+                title: item.title,
+                width: 100,
+                height: 100,
+                thumbnailUrl: item.thumbnailUrl
+            }));
+            setListData(data);
+            setLoading(false);
+        } catch (error) {
+            ToastAndroid.show('Error: ' + error.message, ToastAndroid.SHORT);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    const list = (
+        <MyList
+            data={listData}
+            onPressItem={(item) => ToastAndroid.show("Selected item " + item.id, ToastAndroid.SHORT)}
+        />
+    );
+
+    return (
         <View style={styles.container}>
-            <ListItem title='Test' thumbnailUrl='https://reactnative.dev/img/tiny_logo.png' width={50} height={50}/>
+            <ListItem title='Test' thumbnailUrl='https://reactnative.dev/img/tiny_logo.png' width={50} height={50} />
             <Tile title='Test' thumbnailUrl='https://reactnative.dev/img/tiny_logo.png' width={50} height={50} />
+            {loading ? <Text>Loading ...</Text> : <></>}
+            {listData.length > 0 ? list : <></>}
             <MyList
                 data={photos}
-                renderItem={({item}) => <Tile title={item.title} thumbnailUrl={item.thumbnailUrl} width={item.width} height={item.height}/>}
-                keyExtractor={(item) => item.id}
-                />
-            <Button
-              title='Click'
-              onPress={() => ToastAndroid.show("Selected " + selected.size, ToastAndroid.LONG)}
+                onPressItem={(item) => ToastAndroid.show("Selected item " + item.id, ToastAndroid.SHORT)}
             />
-            <StatusBar style="auto"/>
+            <Button
+                title='Click'
+                onPress={() => ToastAndroid.show("Selected " + selected.size, ToastAndroid.LONG)}
+            />
+            <StatusBar style="auto" />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  circle: (light, circleRadius) => ({
-    borderRadius: circleRadius / 2,
-    borderWidth: 5,
-    borderColor: "blue",
-    width: circleRadius,
-    height: circleRadius,
-    padding: 5,
-    backgroundColor: light ? "blue" : "darkblue",
-  }),
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    circle: (light, circleRadius) => ({
+        borderRadius: circleRadius / 2,
+        borderWidth: 5,
+        borderColor: "blue",
+        width: circleRadius,
+        height: circleRadius,
+        padding: 5,
+        backgroundColor: light ? "blue" : "darkblue",
+    }),
 });
--- components/MyList.js
+++ components/MyList.js
@@ -1,17 +1,23 @@
-import React, {Component, useState} from 'react';
-import {Image, Pressable, View, Text, FlatList, TouchableOpacity} from 'react-native';
-import {ListItem} from './ListItem'
+import React, { Component } from 'react';
+import { FlatList, TouchableOpacity } from 'react-native';
+import { ListItem } from './ListItem';
 
 export class MyList extends Component {
-    state = {selectedId: 0}
+    state = { selectedId: 0 };
+
+    handlePressItem = (item) => {
+        this.setState({ selectedId: item.id });
+        if (this.props.onPressItem) {
+            this.props.onPressItem(item);
+        }
+    };
+
     render() {
         return (
             <FlatList
                 data={this.props.data}
-                renderItem={({item}) =>
-                    <TouchableOpacity
-                        onPress={() => this.setState({selectedId: item.id})}
-                    >
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => this.handlePressItem(item)}>
                         <ListItem
                             title={item.title}
                             width={item.width}
@@ -20,8 +26,8 @@ export class MyList extends Component {
                             selected={item.id == this.state.selectedId}
                         />
                     </TouchableOpacity>
-                }
-                keyExtractor={item => item.id}
+                )}
+                keyExtractor={(item) => item.id.toString()}
             />
         );
     }
